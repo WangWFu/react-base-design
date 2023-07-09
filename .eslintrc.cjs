7cf68996a900082bf439b852249c44f772e4b170
wwf <2643115618@qq.com>
2023-07-09 22:39:59 +0800

    docs: 文件名修改

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,23 @@
 /*eslint no-undef: "error"*/
-/*eslint-env node*/
 module.exports = {
   env: {
     browser: true,
-    es2020: true
+    es2020: true,
+    node: true,
   },
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:react-hooks/recommended", "plugin:storybook/recommended"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:storybook/recommended",
+  ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: "latest",
-    sourceType: "module"
+    sourceType: "module",
   },
   plugins: ["react-refresh"],
   rules: {
-    "react-refresh/only-export-components": "warn"
-  }
+    "react-refresh/only-export-components": "warn",
+  },
 };
